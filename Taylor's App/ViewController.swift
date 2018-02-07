480de8ffdb120201c23f8b04ecd96f73e39c131c
Taylor Gurganus <taylorgurganus@taylors-macbook-pro.local>
2018-02-06 18:46:38 -0800

    Initial Commit

---
+++ Taylor's App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Taylor's App
+//
+//  Created by Taylor Gurganus on 2/6/18.
+//  Copyright © 2018 Taylor Gurganus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
